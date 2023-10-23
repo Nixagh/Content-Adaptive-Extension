80f2a19ad44e5c3e823a5b096340c177fff47fe7
nghia.nguyen-dinh <nghia.nguyen-dinh@banvien.com.vn>
2023-10-23 11:20:06 +0700

    feat: add new AP

--- Content-Adaptive-Extension/scripts/Modules/process/resource/VocabularyWorkShopAdaptive/OnYourOwn/AdaptivePracticeProcess.js
+++ Content-Adaptive-Extension/scripts/Modules/process/resource/VocabularyWorkShopAdaptive/OnYourOwn/AdaptivePracticeProcess.js
@@ -62,6 +62,32 @@ class AdaptivePracticeProcess extends VWAProcess {
         return this.getField("Direction Line", 0);
     }
 
+    setPassage(row) {
+        const directionLine = new Cke("cke_directionLine");
+        const passageContent = new Cke("cke_2_contents");
+        const passageSummary = new Cke("cke_3_contents");
+        const choosePassage = document.querySelectorAll("#questionTypeSelection")[1];
+
+        if (row !== 0 && row !== 24) {
+            const index = row > 23 ? 2 : 1;
+            choosePassage.value = choosePassage.options[index].value;
+            this.getAjaxPassage(choosePassage.value).then(result => {
+                directionLine.setHtml(result.directionLineHTML);
+                passageContent.setHtml(result.passageContentHTML);
+                passageSummary.setHtml(result.passageSummaryHTML);
+            });
+        } else {
+            directionLine.setHtml(row === 0 ? this.getDirectionLineHTML(row) : this.getDirectionLineHTMLOfGo(row % 12));
+            passageContent.setHtml(this.getPassageContent(row));
+            passageSummary.setHtml(this.getPassageSummaryText(row));
+        }
+        console.log("Set passage");
+    }
+
+    getDirectionLineHTMLOfGo(row) {
+        return this.getFieldOfRow("Direction Line", this.getGoTicketSheet()[row]);
+    }
+
     getSetType(row) {
         return row > 23 ? this.adType.GO : row > 11 && row < 24 ? this.adType.B : this.adType.A;
     }
@@ -165,14 +191,14 @@ class AdaptivePracticeProcess extends VWAProcess {
     getCorrectAnswerValueOfGo(row) {
         const options = this.getAnswerGoList(row);
         const correctAnswer = this.getFieldOfRow("Correct Answer", this.getGoTicketSheet()[row]);
-        return String.fromCharCode(97 + options.indexOf(correctAnswer));
+        return String.fromCharCode(97 + options.indexOf(correctAnswer.replaceAll(`"`, "").trim()));
     }
 
     getCorrectAnswerValueOfSetB(row) {
         const data = this.getField("Adaptive Item Answer Choices", row);
         const options = data.split(";").filter(row => row.length > 0).map(row => row.trim());
         const correctAnswer = this.getField("Correct Answer", row);
-        return String.fromCharCode(97 + options.indexOf(correctAnswer));
+        return String.fromCharCode(97 + options.indexOf(correctAnswer.replaceAll(`"`, "").trim()));
     }
 
     getCorrectAnswerValueOfSetA(row) {
