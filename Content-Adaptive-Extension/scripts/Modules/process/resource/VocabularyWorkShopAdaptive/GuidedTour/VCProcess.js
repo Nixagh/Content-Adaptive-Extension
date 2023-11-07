0e2a9ac7b9d5cd3ec8790a93b788a80a05dba0de
nghia.nguyen-dinh <nghia.nguyen-dinh@banvien.com.vn>
2023-11-07 10:27:03 +0700

    fix: Correct Answer Feedback

--- Content-Adaptive-Extension/scripts/Modules/process/resource/VocabularyWorkShopAdaptive/GuidedTour/VCProcess.js
+++ Content-Adaptive-Extension/scripts/Modules/process/resource/VocabularyWorkShopAdaptive/GuidedTour/VCProcess.js
@@ -40,7 +40,7 @@ class VCProcess extends VWAProcess {
                     "Item Standard": this.getFieldOfRow(`Item ${i} Standard`, set),
                     "Item Answer Choices": this.getFieldOfRow(`Item ${i} Answer Choices`, set),
                     "Item Correct Answer": this.getFieldOfRow(`Item ${i} Correct Answer`, set),
-                    "Item Correct Answer Feedback": this.getFieldOfRow(`Item ${i} Correct Answer Feedback`, set),
+                    "Item Correct Answer Feedback": this.getFieldOfRow(`Item ${i} Correct Answer Feedback`, set) || this.getFieldOfRow(`Item ${i} Correct Feedback`, set),
                     "Item Incorrect Feedback 1": this.getFieldOfRow(`Item ${i} Incorrect Feedback 1`, set),
                     "Item Incorrect Feedback 2": this.getFieldOfRow(`Item ${i} Incorrect Feedback 2`, set),
                 }
