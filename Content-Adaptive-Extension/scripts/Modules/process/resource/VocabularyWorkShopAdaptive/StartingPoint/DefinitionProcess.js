8eee6176d8da4954faee271b0de697b51085ee0d
nghia.nguyen-dinh <nghia.nguyen-dinh@banvien.com.vn>
2024-01-12 08:50:47 +0700

    update

--- Content-Adaptive-Extension/scripts/Modules/process/resource/VocabularyWorkShopAdaptive/StartingPoint/DefinitionProcess.js
+++ Content-Adaptive-Extension/scripts/Modules/process/resource/VocabularyWorkShopAdaptive/StartingPoint/DefinitionProcess.js
@@ -1,66 +1,81 @@
 class DefinitionProcess extends VWAProcess {
-	getFullContent() {
-		const definitionsContent = this.getDefinitionSheet();
-		const wordListContent = this.getWordListSheet();
+    getFullContent() {
+        const definitionsContent = this.getDefinitionSheet();
+        const wordListContent = this.getWordListSheet();
 
-		return {first: definitionsContent, second: wordListContent};
-	}
+        return {first: definitionsContent, second: wordListContent};
+    }
 
-	getDefinitionSheet() {
-		const definitionSheetName = `Definitions`;
-		const definitionSheet = this.getSheet(definitionSheetName);
-		const definitionsHeader = this.getHeader(definitionSheet);
-		return this.getContent(definitionSheet, definitionsHeader);
-	}
+    getDefinitionSheet() {
+        const definitionSheetName = `Definitions`;
+        const definitionSheet = this.getSheet(definitionSheetName);
+        const definitionsHeader = this.getHeader(definitionSheet);
+        return this.getContent(definitionSheet, definitionsHeader);
+    }
 
-	getComponentScoreRules(row) {
-		const CSR = {
-			"test": null,
-			"scoringGroups": [
-				{
-					"componentGradingRules": [
-						{
-							"componentId": this.getCID(row),
-							"componentType": "Fill_in_Blank",
-							"componentSubtype": "word",
-							"autoScore": true,
-							"rubricRule": null
-						}
-					],
-					"maxScore": this.getMaxScore()
-				}
-			]
-		}
-		return JSON.stringify(CSR);
-	}
+    getComponentScoreRules(row) {
+        const CSR = {
+            "test": null,
+            "scoringGroups": [
+                {
+                    "componentGradingRules": [
+                        {
+                            "componentId": this.getCID(row),
+                            "componentType": "Fill_in_Blank",
+                            "componentSubtype": "word",
+                            "autoScore": true,
+                            "rubricRule": null
+                        }
+                    ],
+                    "maxScore": this.getMaxScore()
+                }
+            ]
+        }
+        return JSON.stringify(CSR);
+    }
 
-	getAdaptiveAnswerCount() {
-		return 2;
-	}
+    getAdaptiveAnswerCount() {
+        return 2;
+    }
 
-	getQuestionHTML(row) {
-		const inflected = this.getInflectedForm(row);
-		const synonyms = this.getSynonyms(row);
-		const antonyms = this.getAntonyms(row);
-		const exampleSentence = this.getExampleSentence(row);
+    getQuestionHTML(row) {
+        const inflected = this.getInflectedForm(row);
+        const exampleSentence = this.getExampleSentence(row);
+        const synAntBody = this.getSynAntBody(row);
 
-		// replace template [FIB: anno: manual] in question by inputReplace
-		const inputReplace = `<input autocapitalize="off" autocomplete="off" autocorrect="off" cid="${this.getCID(row)}" ctype="Fill_in_Blank" qname="a${row + 1}" spellcheck="false" subtype="word" type="text" />`;
-		const replace = `[${this.getItemType(row)}: anno: ${this.getCorrectWord(row)}]`;
-		const _question = this.beautifulQuestion(exampleSentence).replace(replace, inputReplace);
+        // replace template [FIB: anno: manual] in question by inputReplace
+        const inputReplace = `<input autocapitalize="off" autocomplete="off" autocorrect="off" cid="${this.getCID(row)}" ctype="Fill_in_Blank" qname="a${row + 1}" spellcheck="false" subtype="word" type="text" />`;
+        const replace = `[${this.getItemType(row)}: anno: ${this.getCorrectWord(row)}]`;
 
-		const inflectedForms = Utility.isNotNull(inflected) ? `<div class="inflected-forms">${inflected}</div>` : ``;
-		const synAntBody = Utility.isNotNull(synonyms) || Utility.isNotNull(antonyms) ? `<div class="syn-ant-body">${Utility.isNotNull(synonyms) ? `<b>SYNONYMS </b>${synonyms}` : ""}${Utility.isNotNull(antonyms) ? `<br/><b>ANTONYMS </b>${antonyms}` : ""}</div>` : ``;
-		const question = Utility.isNotNull(_question) ? `<div class="question">${_question}</div>` : ``;
+        const _question = this.beautifulQuestion(exampleSentence).replace(replace, inputReplace);
+        const inflectedForms = Utility.isNotNull(inflected) ? `<div class="inflected-forms">${inflected}</div>` : ``;
+        const question = Utility.isNotNull(_question) ? `<div class="question">${_question}</div>` : ``;
 
-		return `<div class="question-questionStem question-questionStem-1-column">
+        return `<div class="question-questionStem question-questionStem-1-column">
 					<div class="question-stem-content">
 						${inflectedForms}
 						${synAntBody}
 						${question}
 					</div>
 				</div>`;
-	}
+    }
+
+    getSynAntBody(row) {
+        const synonyms = this.getSynonyms(row);
+        const antonyms = this.getAntonyms(row);
+
+        if (!Utility.isNotNull(synonyms) && !Utility.isNotNull(antonyms)) return "";
+
+		const isBoth = Utility.isNotNull(synonyms) && Utility.isNotNull(antonyms);
+        const isSyn = Utility.isNotNull(synonyms);
+        const isAnt = Utility.isNotNull(antonyms);
+
+		const _synonyms = isSyn ? `<b>SYNONYMS </b>${synonyms}` : '';
+        const _antonyms = isAnt ? `<b>ANTONYMS </b>${antonyms}` : '';
+        const _separator = isBoth ? `<br/>` : '';
+
+		return `<div class="syn-ant-body">${_synonyms}${_separator}${_antonyms}</div>`
+    }
 
     getCorrectAnswer(row) {
         const correctAnswer = {
@@ -69,7 +84,7 @@ class DefinitionProcess extends VWAProcess {
                     id: `${this.getCID(row)}`,
                     value: this.getCorrectAnswerValue(row),
                     type: "Fill_in_Blank",
-					subtype: "word"
+                    subtype: "word"
                 }]
         }
         return JSON.stringify(correctAnswer);
@@ -83,56 +98,56 @@ class DefinitionProcess extends VWAProcess {
         return this.getCorrectWord(row);
     }
 
-	checkQuestionContent(questionContent, correctAnswerText, correctText, row) {
-		const inputRex = /<input.*?\/>/g;
+    checkQuestionContent(questionContent, correctAnswerText, correctText, row) {
+        const inputRex = /<input.*?\/>/g;
 
-		const input = questionContent.match(inputRex);
-		if (input === null) {
-			this.createError("questionContent", "Question content does not have input tag", row);
-		} else {
-			const inputCID = input[0].match(/cid="(?<cid>\d+)"/);
-			if (inputCID === null) this.createError("questionContent", "Question content does not have cid attribute", row);
+        const input = questionContent.match(inputRex);
+        if (input === null) {
+            this.createError("questionContent", "Question content does not have input tag", row);
+        } else {
+            const inputCID = input[0].match(/cid="(?<cid>\d+)"/);
+            if (inputCID === null) this.createError("questionContent", "Question content does not have cid attribute", row);
 
-			const inputQName = input[0].match(/qname="(?<qname>.*?)"/);
-			if (inputQName === null) this.createError("questionContent", "Question content does not have qname attribute", row);
-		}
+            const inputQName = input[0].match(/qname="(?<qname>.*?)"/);
+            if (inputQName === null) this.createError("questionContent", "Question content does not have qname attribute", row);
+        }
 
-		// check correct answer text
-		if (correctAnswerText !== this.getCorrectAnswerValue(row)) this.createError("correctAnswerText", "Correct answer text is not correct", row);
-		if(!correctText) this.createError("correctText", "Correct text is empty", row);
-	}
+        // check correct answer text
+        if (correctAnswerText !== this.getCorrectAnswerValue(row)) this.createError("correctAnswerText", "Correct answer text is not correct", row);
+        if (!correctText) this.createError("correctText", "Correct text is empty", row);
+    }
 
-	// ----------------- get field ----------------- //
-	getInflectedForm(row) {
-		return this.getExactlyField("Inflected Forms", row);
-	}
+    // ----------------- get field ----------------- //
+    getInflectedForm(row) {
+        return this.getExactlyField("Inflected Forms", row);
+    }
 
-	getSynonyms(row) {
-		return this.getField("Synonyms", row);
-	}
+    getSynonyms(row) {
+        return this.getField("Synonyms", row);
+    }
 
-	getAntonyms(row) {
-		return this.getField("Antonyms", row);
-	}
+    getAntonyms(row) {
+        return this.getField("Antonyms", row);
+    }
 
-	getExampleSentence(row) {
-		return this.getField("Example Sentence", row);
-	}
+    getExampleSentence(row) {
+        return this.getField("Example Sentence", row);
+    }
 
-	getItemType(row) {
-		return this.getField("Item Type", row);
-	}
+    getItemType(row) {
+        return this.getField("Item Type", row);
+    }
 
-	getCorrectWord(row) {
-		return this.getField("Correct Answer(s)", row);
-	}
+    getCorrectWord(row) {
+        return this.getField("Correct Answer(s)", row);
+    }
 
-	// ----------------- other ----------------- //
-	beautifulQuestion(question) {
-		return question.replace("{", "[").replace("}", "]");
-	}
+    // ----------------- other ----------------- //
+    beautifulQuestion(question) {
+        return question.replace("{", "[").replace("}", "]");
+    }
 
-	getDescription() {
-		return "definitions";
-	}
+    getDescription() {
+        return "definitions";
+    }
 }
