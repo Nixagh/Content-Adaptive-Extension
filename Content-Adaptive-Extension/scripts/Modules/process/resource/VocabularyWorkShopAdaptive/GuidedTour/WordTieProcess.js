218eed48acf5bb250db3473fd1785bbd26b3361b
thuan.pham-van <thuan.pham-van@banvien.com.vn>
2024-09-25 09:38:44 +0700

    update format WordTies

--- Content-Adaptive-Extension/scripts/Modules/process/resource/VocabularyWorkShopAdaptive/GuidedTour/WordTieProcess.js
+++ Content-Adaptive-Extension/scripts/Modules/process/resource/VocabularyWorkShopAdaptive/GuidedTour/WordTieProcess.js
@@ -50,7 +50,8 @@ class WordTieProcess extends VWAProcess {
     getQuestionHTML(row) {
         return `<div class="question-questionStem question-questionStem-1-column">
                     <div class="question-stem-content">
-                    <div class="question">${this.getItem(row)}<br /><br />
+                    <div class="question">${this.getItem(row)}
+                        <br />
                         <div cid="${this.getCID(row)}" ctype="MultipleChoiceAdaptive" layout="Vertical" qname="a${row + 1}" subtype="MSC">
                                 ${this.getOptionHTML(row)}
                         </div>
